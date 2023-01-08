3e56d9ece5b92d7d93b9a292941abcaafdfcc960
Welomeh <>
2023-01-08 13:19:58 +0100

    Prepared test for blast furnace to make stone

--- src/main/java/adris/altoclef/tasks/resources/CollectStoneTask.java
+++ src/main/java/adris/altoclef/tasks/resources/CollectStoneTask.java
@@ -3,11 +3,14 @@ package adris.altoclef.tasks.resources;
 import adris.altoclef.AltoClef;
 import adris.altoclef.TaskCatalogue;
 import adris.altoclef.tasks.ResourceTask;
+import adris.altoclef.tasks.container.CraftInTableTask;
 import adris.altoclef.tasks.container.SmeltInBlastFurnaceTask;
 import adris.altoclef.tasks.container.SmeltInFurnaceTask;
+import adris.altoclef.tasks.movement.DefaultGoToDimensionTask;
 import adris.altoclef.tasksystem.Task;
-import adris.altoclef.util.ItemTarget;
-import adris.altoclef.util.SmeltTarget;
+import adris.altoclef.util.*;
+import adris.altoclef.util.helpers.WorldHelper;
+import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
 import net.minecraft.item.Items;
 import net.minecraft.util.math.BlockPos;
@@ -35,12 +38,27 @@ public class CollectStoneTask extends ResourceTask {
 
     @Override
     protected Task onResourceTick(AltoClef mod) {
-        if (mod.getItemStorage().hasItem(Items.BLAST_FURNACE) ||
-                mod.getBlockTracker().anyFound(Blocks.BLAST_FURNACE) ||
-                mod.getEntityTracker().itemDropped(Items.BLAST_FURNACE)) {
-            return new SmeltInBlastFurnaceTask(new SmeltTarget(new ItemTarget(Items.STONE, _count), new ItemTarget(Items.COBBLESTONE, _count)));
+        if (WorldHelper.getCurrentDimension() == Dimension.OVERWORLD) {
+            if (mod.getModSettings().shouldUseBlastFurnace()) {
+                if (mod.getItemStorage().hasItem(Items.BLAST_FURNACE) ||
+                        mod.getBlockTracker().anyFound(Blocks.BLAST_FURNACE) ||
+                        mod.getEntityTracker().itemDropped(Items.BLAST_FURNACE)) {
+                    return new SmeltInBlastFurnaceTask(new SmeltTarget(new ItemTarget(Items.STONE, _count), new ItemTarget(Items.COBBLESTONE, _count)));
+                }
+                if (_count < 5) {
+                    return new SmeltInFurnaceTask(new SmeltTarget(new ItemTarget(Items.STONE, _count), new ItemTarget(Items.COBBLESTONE, _count)));
+                }
+                //mod.getBehaviour().addProtectedItems(Items.COBBLESTONE, Items.STONE, Items.SMOOTH_STONE);
+                Optional<BlockPos> furnacePos = mod.getBlockTracker().getNearestTracking(Blocks.FURNACE);
+                furnacePos.ifPresent(blockPos -> mod.getBehaviour().avoidBlockBreaking(blockPos));
+                if (mod.getItemStorage().getItemCount(Items.IRON_INGOT) >= 5) {
+                    return TaskCatalogue.getItemTask(Items.BLAST_FURNACE, 1);
+                }
+                return new SmeltInFurnaceTask(new SmeltTarget(new ItemTarget(Items.IRON_INGOT, 5), new ItemTarget(Items.RAW_IRON, 5)));
+            }
+            return new SmeltInFurnaceTask(new SmeltTarget(new ItemTarget(Items.STONE, _count), new ItemTarget(Items.COBBLESTONE, _count)));
         } else {
-            return TaskCatalogue.getItemTask(Items.BLAST_FURNACE, 1);
+            return new DefaultGoToDimensionTask(Dimension.OVERWORLD);
         }
     }
 
@@ -56,6 +74,6 @@ public class CollectStoneTask extends ResourceTask {
 
     @Override
     protected String toDebugStringName() {
-        return "Collecting " + _count + " " + Items.STONE.toString();
+        return "Collecting " + _count + " stone.";
     }
 }
